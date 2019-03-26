a12b7f75ca4424c1f9054895536c7cdc758d7a60
Permamiss <39325797+Permamiss@users.noreply.github.com>
2019-03-26 09:54:45 -0700

    Initial commit

---
+++ HFF_Speedometer/Injection.cs
@@ -0,0 +1,22 @@
+﻿namespace HFF_SpeedTools
+{
+    using UnityEngine;
+
+    public class Injection
+    {
+        public static void Inject()
+        {
+            if (GameObject.Find("SpeedTools"))
+            {
+                foreach (Object toDestroy in SpeedTools.UndestroyedObjects)
+                    Object.Destroy(toDestroy);
+                //Object.Destroy(GameObject.Find("SpeedTools").GetComponent<SpeedTools>());
+                Object.Destroy(GameObject.Find("SpeedTools").gameObject);
+            }
+
+            GameObject newSpeedometer = new GameObject("SpeedTools");
+            Object.DontDestroyOnLoad(newSpeedometer);
+            newSpeedometer.AddComponent<SpeedTools>();
+        }
+    }
+}
